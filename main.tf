7d7cdf9f2544d9f64e440aacdc8b5a818ffd9d9b
lalitha786 <52631492+lalitha786@users.noreply.github.com>
2025-05-30 14:49:32 +0530

    added terraform files

---
+++ main.tf
@@ -0,0 +1,118 @@
+provider "aws" {
+ region = "us-east-1"
+
+}
+
+resource "aws_vpc" "ecs-vpc-786" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "public" {
+  count                   = 2
+  vpc_id                  = aws_vpc.ecs-vpc-786.id
+  cidr_block              = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.ecs-vpc-786.id
+  cidr_block        = "10.0.${count.index + 10}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_internet_gateway" "internet-gw-786" {
+  vpc_id = aws_vpc.ecs-vpc-786.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.ecs-vpc-786.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ecs-vpc-786.id
+  }
+}
+
+resource "aws_route_table_association" "public_association" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+
+}
+
+resource "aws_ecs_cluster" "ecs-cluster-786" {
+  name = "ecs-cluster-786"
+
+
+}
+resource "aws_iam_role" "ecs_task_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+resource "aws_security_group" "ecs_service" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+
+
+resource "aws_lb" "app_alb_786" {
+  name               = "app-alb-786"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ecs_service.id]
+  subnets            = aws_subnet.public[*].id
+}
+
+resource "aws_lb_target_group" "app_tg_786" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+}
+
+resource "aws_lb_listener" "app_listener_786" {
+  load_balancer_arn = aws_lb.app_alb_786.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg_786.arn
+  }
+}
